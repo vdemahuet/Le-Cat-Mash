7729779d4d4403463334c2a682b531dd2d93d16e
Victor de Mahuet <victor.demahuet@gmail.com>
2024-09-21 18:06:59 +0200

    feat(home): Home Page initialized with routing and useless files removed

--- Le-Mash-Cat/src/app/app-routing.module.ts
+++ Le-Mash-Cat/src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+    loadChildren: () => import('./home-page/home-page.module').then(m => m.HomePageModule)
   }
 ];
 @NgModule({

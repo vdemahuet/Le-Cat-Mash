4f099be41c8c2ff911eb1155ff74c04b79e5af24
Victor de Mahuet <victor.demahuet@gmail.com>
2024-09-22 13:53:37 +0200

    feat(home): add banner to home page

--- src/app/pages/home-page/home-page.module.ts
+++ src/app/pages/home-page/home-page.module.ts
@@ -2,14 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './home-page.component';
 import { HomePageRoutingModule} from './home-page-routing.module'
+import {TranslateModule} from "@ngx-translate/core";
+import {IonicModule} from "@ionic/angular";
+import {BannerComponent} from "./banner/banner.component";
 
 
 @NgModule({
-  declarations: [HomePageComponent],
-  imports: [
-    CommonModule,
-    HomePageRoutingModule
-  ],
+  declarations: [HomePageComponent, BannerComponent, BannerComponent],
+    imports: [
+        CommonModule,
+        IonicModule,
+        HomePageRoutingModule,
+        TranslateModule
+    ],
   exports: [HomePageComponent]
 })
 export class HomePageModule { }

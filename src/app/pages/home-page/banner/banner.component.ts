a5ce76d2382544f709b5b07e7b8eb76f298feb73
Victor de Mahuet <victor.demahuet@gmail.com>
2024-09-29 22:28:53 +0200

    feat(home): add scroll animation to go bottom

--- src/app/pages/home-page/banner/banner.component.ts
+++ src/app/pages/home-page/banner/banner.component.ts
@@ -11,4 +11,11 @@ export class BannerComponent  implements OnInit {
 
   ngOnInit() {}
 
+  public scrollTo() {
+    const targetScrollElement = document.getElementById('targetScroll');
+    if (targetScrollElement) {
+      targetScrollElement.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+  }
+
 }
